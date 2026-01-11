48dd6e6402089157103802e6bfedd497cc3784dd
Abhay Ganesh <abhayganesh3329@example.com>
2026-01-12 00:16:11 +0530

    Use env variables for MySQL

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hackathon_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
